ed60565d6214efe495f89f73268e7cf591863587
tegmulhab <tegmulhabt@gmail.com>
2023-02-16 12:55:45 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	flot f;
+	float f;
 	printf("Size of a char:%lu Byte(s)\n",(unsigned long)sizeof(a));
 	printf("Size of an int:%lu Byte(s)\n",(unsigned long)sizeof(b));
 	printf("Size of a long int:%lu Byte(s)\n",(unsigned long)sizeof(c));
