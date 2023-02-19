fa17dc7372d61532ef4439578efdaaa629262b6a
tegmulhab <tegmulhabt@gmail.com>
2023-02-19 12:31:30 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 /** more headers goes there 
-*
-* betty style doc for function main goes there 
-*
-*/
+ * betty style doc for function main goes there 
+ */
 int main(void)
 {
 	int n;
