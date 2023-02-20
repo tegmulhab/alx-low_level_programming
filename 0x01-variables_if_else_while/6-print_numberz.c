b26277627163eac5a3b4d952cda466e64c6cb27a
tegmulhab <tegmulhabt@gmail.com>
2023-02-20 08:47:40 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+		putchar('\n');
+	}
+	return (0);
+}
