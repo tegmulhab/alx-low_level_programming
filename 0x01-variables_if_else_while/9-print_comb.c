07040f9ad0aeb62670aa97e9f85854b3baff6729
tegmulhab <tegmulhabt@gmail.com>
2023-02-20 10:44:40 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 48; n < 57; n++)
 	{
 		putchar(n);
 		putchar(',');
 		putchar(' ');
 	}
-	putchar('\n');
+	putchar(n);
 	return (0);
 }
