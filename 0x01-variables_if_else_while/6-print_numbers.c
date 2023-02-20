5dce6e37bd9eb1afe0a2ff3aaf5948fe48b52cd3
tegmulhab <tegmulhabt@gmail.com>
2023-02-20 09:13:29 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+        int n = 48;
+
+        while (n <= 57)
+        {
+                putchar(n);
+                n++;
+                putchar('\n');
+        }
+        putchar('\n');
+        return (0);
+}
