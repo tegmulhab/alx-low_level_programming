47bb8d81fe37fd1f9153428b22b069f4d4d15b9a
tegmulhab <tegmulhabt@gmail.com>
2023-02-21 12:10:16 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,24 @@
 #include "main.h"
-#include <stdlib.h>
 /**
- * _abs - checks if the alphabet is a lowercase
- * @n: Description of parameter c (this is an integer parameter)
- * Return: Always 0.
-*/
-int _abs(int n)
+ * print_last_digit - prints the last digit of a number
+ * @n: a parameter used to recieve a value to be computerd by the calling program
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
 {
-	return (abs(n));
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+
 }
+
