1a84a78a0f9116b8ce5a6d5a5b5810cee30d9961
tegmulhab <tegmulhabt@gmail.com>
2023-02-21 11:45:18 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * _abs - checks if the alphabet is a lowercase
+ * @n: Description of parameter c (this is an integer parameter)
+ * Return: Always 0.
+*/
+int _abs(int n)
+{
+	return (abs(n));
+}
