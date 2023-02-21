3f472b372aaed85563c584e8b9fe7e7c5922d059
tegmulhab <tegmulhabt@gmail.com>
2023-02-21 09:06:39 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,14 @@
 void print_alphabet_x10(void)
 {
 	char letter;
+	int n;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (n = 0; n <= 9; n++)
 	{
-		_putchar(letter);
-	}
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
 	_putchar('\n');
+	}
 }
