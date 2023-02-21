2456383b7bccf4f8200c80d9e96e341b91192b2f
tegmulhab <tegmulhabt@gmail.com>
2023-02-21 12:03:32 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - checks if the alphabet is a lowercase
+ * @n: Description of parameter c (this is an integer parameter)
+ * Return: Always 0.
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+
+}
