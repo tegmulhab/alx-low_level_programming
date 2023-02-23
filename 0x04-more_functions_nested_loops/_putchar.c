7a323e381242706dfad97ab8ba97a04d5f9750bc
tegmulhab <tegmulhabt@gmail.com>
2023-02-23 09:14:27 -0800

    3-print_numbers.c:

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -2,11 +2,11 @@
 
 /**
 *_putchar - write the character c to stdout
-*@c:The character to print
-*Return 1 if true, 0 if false
+*@c: The character to print
+*Return: 1 if true, 0 if false
 *On error, -1 return.
 */
-int_putchar(char c)
+int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
