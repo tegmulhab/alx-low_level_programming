3ee96c771d5148b957a2baf03683a56350c109d1
tegmulhab <tegmulhabt@gmail.com>
2023-02-27 07:43:48 -0800

    1-main.c

--- alx-low_level_programming/1-swap.c
+++ alx-low_level_programming/1-swap.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
- * @n: input
- * Return: n
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
  */
-void reset_to_98(int *n)
+void swap_int(int *a, int *b)
 {
-	*n = 98;
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
 }
 
