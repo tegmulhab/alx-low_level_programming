190726b3a15f881fc124fc0f67a0a58a02635c8f
tegmulhab <tegmulhabt@gmail.com>
2023-02-27 08:33:29 -0800

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+*_putchar - write the character c to stdout
+*@c:The character to print
+*Return  0 
+*
+*/
+int _putchar(char c)
+{
+putchar(c);
+return (0);
+}
+
